7daa973d2e4876c1ec40a9ba94b208847855fb8d
Lucas Grönborg <lucas94gronborg@gmail.com>
2019-02-02 19:37:40 +0100

    Restructured the project

---
+++ dev/index.php
@@ -0,0 +1,256 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Grönborg</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:500,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Muli:400,400i,800,800i" rel="stylesheet">
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/resume.css" rel="stylesheet">
+
+  </head>
+
+  <body id="page-top">
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
+      <a class="navbar-brand js-scroll-trigger" href="#page-top">
+        <span class="d-block d-lg-none">Lucas Grönborg</span>
+        <span class="d-none d-lg-block">
+          <img class="img-fluid img-profile rounded-circle mx-auto mb-2" src="img/profile.jpg" alt="">
+        </span>
+      </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#about">About</a>
+          </li>
+          <li class="nav-item">
+           <a class="nav-link js-scroll-trigger" href="#education">Education</a>
+          </li>
+          <li class="nav-item">
+           <a class="nav-link js-scroll-trigger" href="#skills">Skills</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#projects">Projects</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#interests">Interests</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#awards">Awards</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    <div class="container-fluid p-0">
+
+      <section class="resume-section p-3 p-lg-5 d-flex d-column" id="about">
+        <div class="my-auto">
+          <h1 class="mb-0">Lucas
+            <span class="text-primary">Grönborg</span>
+          </h1>
+          <div class="subheading mb-5">(+46) 72 300 90 20 ·
+            <a href="mailto:gronborg@kth.se">gronborg@kth.se</a>
+          </div>
+          <p class="lead mb-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+             Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+          <div class="social-icons">
+            <a href="https://www.linkedin.com/in/lucas-gr%C3%B6nborg-62023ba6/" target="_blank">
+              <i class="fab fa-linkedin-in"></i>
+            </a>
+            <a href="https://github.com/Greencastle94" target="_blank">
+              <i class="fab fa-github"></i>
+            </a>
+          </div>
+        </div>
+      </section>
+
+      <hr class="m-0">
+
+      <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="education">
+        <div class="my-auto">
+          <h2 class="mb-5">Education</h2>
+
+          <div class="resume-item d-flex flex-column flex-md-row mb-5">
+            <div class="resume-content mr-auto">
+              <h3 class="mb-0">KTH - Royal Institute of Technology</h3>
+              <div class="subheading mb-3">Master of Science</div>
+              <div>Interactive Media Technology - Visual Media Track</div>
+              <p>GPA: -</p>
+            </div>
+            <div class="resume-date text-md-right">
+              <span class="text-primary">August 2017 - Now</span>
+            </div>
+          </div>
+
+          <div class="resume-item d-flex flex-column flex-md-row">
+            <div class="resume-content mr-auto">
+              <h3 class="mb-0">KTH - Royal Institute of Technology</h3>
+              <div class="subheading mb-3">Bachelor of Science</div>
+              <div>Media Technology - Computer Science Track</div>
+              <p>GPA: -</p>
+            </div>
+            <div class="resume-date text-md-right">
+              <span class="text-primary">August 2014 - June 2017</span>
+            </div>
+          </div>
+
+        </div>
+      </section>
+
+      <hr class="m-0">
+
+      <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="skills">
+        <div class="my-auto">
+          <h2 class="mb-5">Skills</h2>
+
+          <div class="subheading mb-3">Programming Languages &amp; Tools</div>
+          <ul class="list-inline dev-icons">
+            <li class="list-inline-item">
+              <i class="fab fa-html5"></i>
+            </li>
+            <li class="list-inline-item">
+              <i class="fab fa-css3-alt"></i>
+            </li>
+            <li class="list-inline-item">
+              <i class="fab fa-js-square"></i>
+            </li>
+            <li class="list-inline-item">
+              <i class="fab fa-python"></i>
+            </li>
+            <li class="list-inline-item">
+              <i class="fab fa-java"></i>
+            </li>
+          </ul>
+
+          <div class="subheading mb-3">Lorem ipsum</div>
+          <ul class="fa-ul mb-0">
+            <li>
+              <i class="fa-li fa fa-check"></i>
+               Dolor sit amet</li>
+            <li>
+              <i class="fa-li fa fa-check"></i>
+              Consectetur adipiscing elit </li>
+            <li>
+              <i class="fa-li fa fa-check"></i>
+              Sed do eiusmod tempor incididunt</li>
+            <li>
+              <i class="fa-li fa fa-check"></i>
+              Ut labore et dolore magna aliqua</li>
+          </ul>
+        </div>
+      </section>
+
+      <hr class="m-0">
+
+      <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="projects">
+        <div class="my-auto">
+          <h2 class="mb-5">Projects</h2>
+
+          <div class="resume-item d-flex flex-column flex-md-row mb-5">
+            <div class="resume-content mr-auto">
+              <h3 class="mb-0 text-capitalize project-title">
+                 <a href="https://kasperii.github.io/mutemike/" target="_blank">Mute Mike II</a>
+              </h3>
+              <div class="subheading mb-3">Game · VR · Point & Click Mystery</div>
+              <p>A mobile VR experience where you navigate a creepy cellar to solve a murder mystery.
+                 The game consisted of several 270 degree videos stitched together
+                  using simple interaction.
+                 In this project I worked with the interaction, both the design and development part of it.</p>
+            </div>
+            <div class="resume-date text-md-right">
+              <span class="text-primary">Nov 2018 - Jan 2019</span>
+            </div>
+          </div>
+
+          <div class="resume-item d-flex flex-column flex-md-row mb-5">
+            <div class="resume-content mr-auto">
+              <h3 class="mb-0 text-capitalize project-title">
+                 <a href="https://kasperii.github.io/beBeam/" target="_blank">BeBeam (a.k.a. BeamAR)</a>
+              </h3>
+              <div class="subheading mb-3">Game · AR · Co-Op Puzzle</div>
+              <p>A course project where I was responsible for the multiplayer aspect of the game and supposed to make virtual objects to synchronize between mobile units.
+                 I succeded but because of time constraints the multiplayer didn't make it in the final prototype.</p>
+            </div>
+            <div class="resume-date text-md-right">
+              <span class="text-primary">Aug 2018 - Nov 2018</span>
+            </div>
+          </div>
+
+          <div class="resume-item d-flex flex-column flex-md-row mb-5">
+            <div class="resume-content mr-auto">
+              <h3 class="mb-0 text-capitalize project-title">
+                 <a href="https://youtu.be/UVxmRhnPgQk" target="_blank">ARmborst</a>
+              </h3>
+              <div class="subheading mb-3">Game · AR · Motion Gesture</div>
+              <p>The goal of the project was to explore new ways of interaction in Smart Device Augmented Reality (SDAR).
+                 We found motion gesture to be a candidate and developed an AR crossbow game.
+                 I worked mostly with the software development of the prototype but I also conducted and evaluated user tests.</p>
+            </div>
+            <div class="resume-date text-md-right">
+              <span class="text-primary">Nov 2017 - Jan 2018</span>
+            </div>
+          </div>
+
+        </div>
+
+      </section>
+
+      <hr class="m-0">
+
+      <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="interests">
+        <div class="my-auto">
+          <h2 class="mb-5">Interests</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+             Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+          <p class="mb-0">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+             Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+      </section>
+
+      <hr class="m-0">
+
+      <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="awards">
+        <div class="my-auto">
+          <h2 class="mb-5">Awards &amp; Certifications</h2>
+          <ul class="fa-ul mb-0">
+            <li>
+               <a href="https://www.stagecast.io/livehacks/lh3-hack-the-show-with-stagecast" target="_blank">
+                 <i class="fa-li fa fa-trophy text-warning"></i>
+                 Stagecast's Livehacks #3 - Best Project</li>
+               </a>
+        </div>
+      </section>
+
+    </div>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for this template -->
+    <script src="js/resume.min.js"></script>
+
+  </body>
+
+</html>
